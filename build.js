3a5e5b3a6ecf9f9582faac6b68dd0fdeb63edf5a
ne_Sachirou <utakata.c4se@gmail.com>
2015-11-30 01:42:38 +0900

    Build script.

---
+++ build.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env babel-node
+'use strict';
+
+import * as babel from 'babel-core';
+import fs         from 'fs';
+
+const COMPONENTS = {
+  sprintf : true,
+  strftime: false,
+  debug   : false,
+};
+
+function promisify(func) {
+  return function (...args) {
+    var me = this;
+    return new Promise((resolve, reject) => {
+      function done(err, ...args) {
+        if (err) {
+          return reject(err);
+        }
+        resolve.apply(me, args);
+      }
+
+      args.push(done);
+      func.apply(me, args);
+    });
+  };
+}
+
+Promise.all([
+    promisify(fs.readFile)('sprintf.js', {encoding: 'utf8'}),
+    promisify(fs.readFile)('package.json', {encoding: 'utf8'}),
+  ]).
+  then((values) => {
+    var code   = values[0];
+    var config = JSON.parse(values[1]).babel;
+    for (let compoment in COMPONENTS) if (COMPONENTS.hasOwnProperty(compoment)) {
+      if (!COMPONENTS[compoment]) {
+        code = code.replace(new RegExp(`^// {{{ !${compoment}(?:.|\n)*^// }}} !${compoment}`, 'mg'), '');
+      }
+    }
+    code = babel.transform(code, config).code;
+    return promisify(fs.writeFile)('sprintf.min.js', code);
+  }).
+  catch((err) => console.error(err));
